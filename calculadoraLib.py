4320436f4ae61a978304e5f054de4488f6f86eb0
crisalex22 <60363924+crisalex22@users.noreply.github.com>
2020-05-24 20:11:14 -0500

    Update calculadoraLib.py
    Se implementa la funcio raiz() con la aproximacion de Bakhshali para calcular la raiz cuadrada de un numero

--- calculadoraLib.py
+++ calculadoraLib.py
@@ -9,4 +9,23 @@ def multiplicacion(a,b):
 
 def division(a,b):
 		return a/b
+def raiz(s): 
+    pSq = 0;  
+    N = 0;  
+    for i in range(int(s), 0, -1): 
+        for j in range(1, i): 
+            if (j * j == i): 
+                pSq = i; 
+                N = j; 
+                break; 
+      
+        if (pSq > 0): 
+            break; 
+  
+    d = s - pSq;     # calculate d 
+    P = d / (2.0 * N); # calculate P 
+    A = N + P; # calculate A 
+    # calculate sqrt(S). 
+    sqrt_of_s = A - ((P * P) / (2.0 * A));  
+    return sqrt_of_s; 
 
