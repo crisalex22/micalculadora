b28a126f2de1ced6ce8be2927715e57e47d44334
crisalex22 <60363924+crisalex22@users.noreply.github.com>
2020-05-24 20:04:20 -0500

    Update test.py
    Se implementa la funcion test_resta() para testear la funcion resta de calculadoraLib.py

--- test.py
+++ test.py
@@ -13,6 +13,20 @@ def test_suma():
 		else:
 			print("Error de la funcion suma deberia ser ",v[0]," + ",v[1]," = ",vectorespuesta[n],"pero se tiene almacenado ",v[2])
 
+def test_resta():
+        #arrange
+    vectordata=[[5,3,2],[6,3,3],[3,-1,4]]
+    vectorespuesta=[]
+    #act
+    for n,v in enumerate(vectordata):
+        vectorespuesta.append(resta(v[0],v[1]))
+    #assert
+        if v[2]==vectorespuesta[n]:
+            print("La funcion resta es correcta: ",v[0]," + ", v[1]," =",vectorespuesta[n])
+        else:
+            print("Error de la funcion resta deberia ser ",v[0]," + ",v[1]," = ",vectorespuesta[n],"pero se tiene almacenado ",v[2])
 
 if __name__ == '__main__':
 	test_suma()
+	test_resta()
+	
